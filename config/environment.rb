492ff7a2b3f2360c8ea583d3e224ecccc236fc53
Lauris Vecvagars <l.vecvagars@outlook.com>
2019-06-13 18:24:32 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,14 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
+Rails.application.initialize
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'glacial-retreat-75050.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
